c600d848b48a1eea108f5540e2b9eeaef4eb2e27
Soykat Amin <soykatamin@gmail.com>
2024-07-22 15:37:41 +0200

    Fixed form issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
     helper_method :current_user
-  
+    protect_from_forgery with: :exception
+
     private
-  
+
     def current_user
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
   end
-  
