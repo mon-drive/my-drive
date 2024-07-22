473d1944f86cf4ed92660166b8edc1ad1ab43587
Soykat Amin <soykatamin@gmail.com>
2024-07-22 15:09:27 +0200

    Added payment page. Added buttons for plan selection in pricing page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,21 @@ class PagesController < ApplicationController
   def pricing
     # logica per la pagina di pricing, se necessaria
   end
+  def payment
+    @plan = params[:plan]
+    if @plan.nil?
+      redirect_to pricing_path, alert: 'Nessun piano selezionato.'
+    end
+  end
+  def payment_complete
+    plan = params[:plan]
+    # Logica per completare l'iscrizione, ad esempio:
+    # - Aggiornare l'utente con il piano selezionato
+    # - Inviare una conferma via email
+    # - Eseguire altre azioni necessarie
+
+    # Redirect alla homepage o a una pagina di conferma
+    redirect_to root_path, notice: "Iscrizione completata con successo."
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 Rails.application.routes.draw do
+
   root 'home#index'
+
+  # Rotte per OmniAuth
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   get 'auth/failure', to: redirect('/')
+
+  # Rotta per logout
   get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  # Rotta per caricare file e visualizzare la pagina di pricing
   post 'upload', to: 'drive#upload'
   get 'pricing', to: 'pages#pricing'
+
+  # Rotta per la pagina di pagamento
+  get 'payment', to: 'pages#payment', as: 'payment'
+
+  # Rotta per completare il pagamento
+  post 'payment_complete', to: 'pages#payment_complete', as: 'payment_complete'
+
 end
