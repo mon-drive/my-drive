87f68a6747776d6222ec95c8d93285241ae03035
Soykat Amin <soykatamin@gmail.com>
2024-07-23 15:07:53 +0200

    Cooked bootstrap icons. Fixed common navbar bug that aligned from right integrated items. Added turbolinks for fast navigation between pages.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@
 //= require bootstrap-sprockets
 
 import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
@@ -15,3 +16,4 @@ import "bootstrap-icons/font/bootstrap-icons.css"
 
 Rails.start()
 ActiveStorage.start()
+Turbolinks.start()
