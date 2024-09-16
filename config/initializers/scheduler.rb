22b0f7b33e537cf25ca2f1d4805c936fce9939c3
Soykat Amin <soykatamin@gmail.com>
2024-09-16 19:11:06 +0200

    Bug fix and reintegration of scheduled update
    Fixed delete item error when not in google drive. Reintegrated scheduled update and scheduled for an update every 2 minutes.

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,7 +2,7 @@ require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler.singleton
 
-scheduler.every '1m' do
+scheduler.every '2m' do
   User.find_each do |user|
     if user.logged_in?
       DriveController.new.update_db(user)
